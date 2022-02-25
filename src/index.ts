c0b850e7468e831716716ae57bc2b260035d3d20
Emmanuel Dushime <64155652+dushimeemma@users.noreply.github.com>
2022-02-25 14:13:40 +0200

    task(reverse): string (#1) (#2)
    - added reverse string function
    
    [Delivers N/A]

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import { reverseStr } from './challenges/reveserve_string';
+import { reverseStr } from './challenges/reverse_string';
+import { reverseNum } from './challenges/reverse_number';
 
 console.log(reverseStr('dushime'));
+console.log(reverseNum(5432));
